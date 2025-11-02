0fa91569a025ff4636015e8f31ecd1db0eb95de2
Moreskyv <bluecray@bk.ru>
2025-11-02 17:33:02 +0900

    debug

--- src/count.jsx
+++ src/count.jsx
@@ -1,6 +1,6 @@
-import CountUp from './CountUp.jsx'
-import './countUp.css'
-import AnimatedContent from './AnimatedContent/AnimatedContent.jsx'
+import CountUp from './CountUp.jsx';
+import './count.css';
+import AnimatedContent from './AnimatedContent/AnimatedContent.jsx';
 
 const Landing = () => {
   const handleAnimationComplete = () => {
@@ -8,36 +8,36 @@ const Landing = () => {
   };
 
   return (
-    <>
-     <div className='count-text-box'>
-
-
-<AnimatedContent
-  distance={150}
-  direction="vertical"
-  reverse={false}
-  duration={1}
-  ease="power3.out"
-  initialOpacity={0.2}
-  animateOpacity
-  scale={1}
-  threshold={0.2}
-  delay={0}
->
-  <div>
-    <p><CountUp
-  from={0}
-  to={50}
-  separator=","
-  direction="up"
-  duration={1}
-  className="count-up-text"/>+ <br /> реализованных проектов</p>
-
-  </div>
-</AnimatedContent>
-
-     </div>
-    </>
+    <div className="landing-container">
+      <AnimatedContent
+        distance={150}
+        direction="vertical"
+        reverse={false}
+        duration={1}
+        ease="power3.out"
+        initialOpacity={0.2}
+        animateOpacity
+        scale={1}
+        threshold={0.2}
+        delay={0}
+        onComplete={handleAnimationComplete}
+      >
+        <div className="count-text-box">
+          <p className="count-text">
+            <CountUp
+              from={0}
+              to={50}
+              separator=","
+              direction="up"
+              duration={1}
+              className="count-up-text"
+            />{' '}
+            + <br />
+            реализованных проектов
+          </p>
+        </div>
+      </AnimatedContent>
+    </div>
   );
 };
 
