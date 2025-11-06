5710253cb8fb6d2c91b6b331a1f7f40300ffae65
Moreskyv <bluecray@bk.ru>
2025-11-06 17:51:34 +0900

    player

--- src/player.jsx
+++ src/player.jsx
@@ -1,57 +1,74 @@
 import React, { useState } from "react";
-import './player.css'
-const videoList = [
-  {
-    title: "Видео 1",
-    embedUrl: "https://kinescope.io/9o5sfWEzNrDAbszmBTdG7F",
-  },
-  {
-    title: "Видео 2",
-    embedUrl: "https://rutube.ru/play/embed/VIDEO_ID_2",
-  },
-  {
-    title: "Видео 3",
-    embedUrl: "https://rutube.ru/play/embed/VIDEO_ID_3",
-  },
+import "./player.css";
+
+const reelsVideos = [
+  { title: "Reel 1", embedUrl: "https://kinescope.io/9o5sfWEzNrDAbszmBTdG7F" },
+  { title: "Reel 2", embedUrl: "https://kinescope.io/m6TrE82mbLKiLs1Um9eEym" },
+  { title: "Reel 3", embedUrl: "https://kinescope.io/dqd6Gz85FjFNToWviAoN34" },
+  { title: "Reel 4", embedUrl: "https://kinescope.io/nKhCKCmUqsnyBgdfbnSyyH" },
+];
+
+const motionVideos = [
+  { title: "Motion 1", embedUrl: "https://kinescope.io/ar8L3CL9xEAk5PE2BSh2ji" },
+  { title: "Motion 2", embedUrl: "https://kinescope.io/fTKKNJ18KYuzNfMXMtj3MT"},
+  { title: "Motion 3", embedUrl: "https://kinescope.io/w44gfAGUpVr16UXR4TgPBW" },
+  { title: "Motion 4", embedUrl: "https://kinescope.io/62PiMdZ56s2j8bdVGQJiU6" },
+];
+
+const horizontalVideos = [
+  { title: "Horizontal 1", embedUrl: "https://kinescope.io/gSFZKa9L5q4TzhEsvhmtqY" },
+  { title: "Horizontal 2", embedUrl: "https://kinescope.io/sKbkn79ddJjbskrhbUHLFo" },
 ];
 
 export default function VideoPlayer() {
-  const [currentVideo, setCurrentVideo] = useState(videoList[0]);
+  const categories = {
+    Reels: reelsVideos,
+    Motion: motionVideos,
+    Horizontal: horizontalVideos,
+  };
+
+  const [activeCategory, setActiveCategory] = useState("Reels");
+  const [currentVideo, setCurrentVideo] = useState(categories["Reels"][0]);
 
   return (
-    <div style={{ maxWidth: "840px", margin: "20px auto", textAlign: "center" }}>
-      <p className="player-text">Наши работы:</p>
-      <div style={{ position: "relative", paddingBottom: "56.25%", height: 0 }}>
+    <div className="player-wrapper">
+      <p className="player-title">Наши работы</p>
+
+      {/* Переключение категорий */}
+      <div className="tabs">
+        {Object.keys(categories).map((cat) => (
+          <button
+            key={cat}
+            className={`tab-button ${activeCategory === cat ? "active" : ""}`}
+            onClick={() => {
+              setActiveCategory(cat);
+              setCurrentVideo(categories[cat][0]);
+            }}
+          >
+            {cat}
+          </button>
+        ))}
+      </div>
+
+      {/* Видео */}
+      <div className="video-container">
         <iframe
           src={currentVideo.embedUrl}
           frameBorder="0"
           allowFullScreen
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-          }}
           title={currentVideo.title}
         ></iframe>
       </div>
 
-          
-      <div style={{ marginTop: "20px" }}>
-        {videoList.map((video, index) => (
+      {/* Кнопки выбора видео */}
+      <div className="video-selector">
+        {categories[activeCategory].map((video) => (
           <button
-            key={index}
+            key={video.title}
+            className={`video-button ${
+              video.embedUrl === currentVideo.embedUrl ? "selected" : ""
+            }`}
             onClick={() => setCurrentVideo(video)}
-            style={{
-              margin: "5px",
-              padding: "10px 20px",
-              cursor: "pointer",
-              backgroundColor: video.embedUrl === currentVideo.embedUrl ? "#010246ff" : "#000000ff",
-              color: "white",
-              border: "none",
-              borderRadius: "5px",
-            }}
           >
             {video.title}
           </button>
